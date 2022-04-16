6b5918c542dbc638846a4d05ce76931ff18d70ab
Rehan Ayub <rehanayub20@gmail.com>
2022-04-15 21:02:14 -0400

    Initial commit

---
+++ blackjack.py
@@ -0,0 +1,13 @@
+# RULES:
+#   Try to get as close to 21 without going over.
+#   Kings, Queens, Jacks worth 10 points.
+#   Aces worth 1 or 11 points.
+#   Cards 2 through 10 are worth their face value
+#   (H)it to take another card.
+#   (S)tand to stop taking cards.
+#   On your first play, you can (D)ouble-down to increase your bet but must hit exactly one more time
+#   before standing.
+#   In case of a tie, the bet is returned to the player.
+#   The dealer stops hitting at 17.
+
+import random, sys
