79b00d75478363089e731c5a0bd21479915ff0d4
Rehan Ayub <rehanayub20@gmail.com>
2022-04-15 21:26:21 -0400

    Add getBet method

--- blackjack.py
+++ blackjack.py
@@ -11,3 +11,49 @@
 #   The dealer stops hitting at 17.
 
 import random, sys
+
+#Set up the constants
+HEARTS = chr(9829)
+DIAMONDS = chr(9830)
+SPADES = chr(9824)
+CLUBS = chr(9827)
+
+def main():
+    print("Welcome to Blackjack - PyTerminal Edition")
+
+    money = 2000
+    while True:
+        #Check if player has run out of money
+        if money <= 0:
+            print("You're broke!")
+            print("Good thing you weren't playing with real money.")
+            print("Thanks for playing!")
+            sys.exit()
+        
+        #Player to enter their bet for this round
+        bet = getBet(money)
+
+
+def getBet(maxBet):
+    """Ask the player how much they want to bet for this round."""
+    while True: #keep asking until they enter a valid amount
+        print('How much do you bet? (1-{}, or QUIT)'.format(maxBet))
+        bet = input('> ').upper().strip()
+        
+        if bet == 'QUIT':
+            print("Thanks for playing!")
+            sys.exit()
+        
+        if not bet.isdecimal():
+            continue #if the player didn't enter a number, ask again
+        
+        bet = int(bet)
+        if 1 <= bet <= maxBet:
+            return bet
+        
+        print("You don't have enough money!")
+
+
+
+if __name__ == "__main__":
+        main()
